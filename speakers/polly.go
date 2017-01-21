5b031cccb4e5ac9911a4d8a96a89e9f7fdd2ee69
thisisaaroland <aaron@mapzen.com>
2017-01-20 19:37:31 -0800

    make the polly speaker actually return something - still lots to do

--- speakers/polly.go
+++ speakers/polly.go
@@ -6,18 +6,24 @@ import (
 	"github.com/aws/aws-sdk-go/service/polly"
 	"io"
 	"io/ioutil"
-	"log"
 	"strings"
 )
 
 type PollySpeaker struct {
 	Speaker
-	service *polly.Polly
+	service      *polly.Polly
+	OutputFormat string
+	VoiceId      string
 }
 
 func NewPollySpeaker() (*PollySpeaker, error) {
 
-	sess, err := session.NewSession()
+	// please fix me - this assumes shared credentials
+
+	cfg := aws.NewConfig()
+	cfg.WithRegion("us-east-1") // please fix me...
+
+	sess, err := session.NewSession(cfg)
 
 	if err != nil {
 		return nil, err
@@ -26,7 +32,9 @@ func NewPollySpeaker() (*PollySpeaker, error) {
 	svc := polly.New(sess)
 
 	s := PollySpeaker{
-		service: svc,
+		service:      svc,
+		OutputFormat: "mp3",
+		VoiceId:      "Russell",
 	}
 
 	return &s, nil
@@ -46,30 +54,40 @@ func (s *PollySpeaker) WriteString(text string) (int64, error) {
 
 func (s *PollySpeaker) Write(p []byte) (int, error) {
 
+	// https://docs.aws.amazon.com/polly/latest/dg/API_SynthesizeSpeech.html
+
 	params := &polly.SynthesizeSpeechInput{
-		OutputFormat: aws.String("OutputFormat"), // Required
-		Text:         aws.String("Text"),         // Required
-		VoiceId:      aws.String("VoiceId"),      // Required
-		LexiconNames: []*string{
-			aws.String("LexiconName"), // Required
-			// More values...
-		},
-		SampleRate: aws.String("SampleRate"),
-		TextType:   aws.String("TextType"),
+		OutputFormat: aws.String(s.OutputFormat),
+		VoiceId:      aws.String(s.VoiceId),
+		Text:         aws.String(string(p)),
 	}
 
 	resp, err := s.service.SynthesizeSpeech(params)
-
-	log.Println("ERR")
-	log.Println(err)
 
 	if err != nil {
 		return 0, err
 	}
 
-	log.Println(resp)
+	// please fix me - can feed all the resp.AudioStream results
+	// to a single filehandle or are there MP3 header things that
+	// can't be repeated... TBD
 
-	return 0, nil
+	b, err := ioutil.ReadAll(resp.AudioStream)
+
+	if err != nil {
+		return 0, err
+	}
+
+	err = ioutil.WriteFile("test.mp3", b, 0644)
+
+	if err != nil {
+		return 0, err
+	}
+
+	// please fix me - cast *int64 as int
+	// return int(resp.RequestCharacters), nil
+
+	return 1, nil
 }
 
 func (s *PollySpeaker) Close() error {
