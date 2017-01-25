c7d68242cbf96a61bf28a0bf99235c7be8171447
thisisaaroland <aaron@mapzen.com>
2017-01-24 18:35:02 -0800

    defer all polly recording until the end

--- speakers/polly.go
+++ speakers/polly.go
@@ -1,11 +1,14 @@
 package speakers
 
 import (
+	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/polly"
 	"io"
 	"io/ioutil"
+	_ "log"
+	"os"
 	"strings"
 )
 
@@ -14,6 +17,8 @@ type PollySpeaker struct {
 	service      *polly.Polly
 	OutputFormat string
 	VoiceId      string
+	Filename     string
+	buffer       []string
 }
 
 func NewPollySpeaker() (*PollySpeaker, error) {
@@ -29,12 +34,16 @@ func NewPollySpeaker() (*PollySpeaker, error) {
 		return nil, err
 	}
 
+	buffer := make([]string, 0)
+
 	svc := polly.New(sess)
 
 	s := PollySpeaker{
 		service:      svc,
 		OutputFormat: "mp3",
 		VoiceId:      "Russell",
+		Filename:     "polly",
+		buffer:       buffer,
 	}
 
 	return &s, nil
@@ -54,42 +63,46 @@ func (s *PollySpeaker) WriteString(text string) (int64, error) {
 
 func (s *PollySpeaker) Write(p []byte) (int, error) {
 
+	s.buffer = append(s.buffer, string(p))
+	return len(p), nil
+}
+
+func (s *PollySpeaker) Record() error {
+
+	fname := fmt.Sprintf("%s.%s", s.Filename, s.OutputFormat)
+
+	fh, err := os.OpenFile(fname, os.O_RDWR|os.O_CREATE, 0644)
+	defer fh.Close()
+
+	if err != nil {
+		return err
+	}
+
 	// https://docs.aws.amazon.com/polly/latest/dg/API_SynthesizeSpeech.html
+
+	text := strings.Join(s.buffer, "\n\n")
 
 	params := &polly.SynthesizeSpeechInput{
 		OutputFormat: aws.String(s.OutputFormat),
 		VoiceId:      aws.String(s.VoiceId),
-		Text:         aws.String(string(p)),
+		Text:         aws.String(text),
 	}
 
 	resp, err := s.service.SynthesizeSpeech(params)
 
 	if err != nil {
-		return 0, err
+		return err
 	}
 
-	// please fix me - can feed all the resp.AudioStream results
-	// to a single filehandle or are there MP3 header things that
-	// can't be repeated... TBD
-
-	b, err := ioutil.ReadAll(resp.AudioStream)
+	_, err = io.Copy(fh, resp.AudioStream)
 
 	if err != nil {
-		return 0, err
+		return err
 	}
 
-	err = ioutil.WriteFile("test.mp3", b, 0644)
-
-	if err != nil {
-		return 0, err
-	}
-
-	// please fix me - cast *int64 as int
-	// return int(resp.RequestCharacters), nil
-
-	return 1, nil
+	return nil
 }
 
 func (s *PollySpeaker) Close() error {
-	return nil
+	return s.Record()
 }
